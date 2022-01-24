6c7cbe1f681f5e026ca134d3a6646f45d212f82f
Mehmet Fırat Kahvecioğlu <71196378+Fratos07@users.noreply.github.com>
2022-01-24 22:58:38 +0300

    add firebase

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
+import auth from '@react-native-firebase/auth';
 
 const App = () => {
+  const handleAuth = () => {
+    auth()
+      .createUserWithEmailAndPassword(
+        'jane.doe@example.com',
+        'SuperSecretPassword!',
+      )
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+  const handleSignIn = () => {
+    auth()
+      .signInWithEmailAndPassword(
+        'jane.doe@example.com',
+        'SuperSecretPassword!',
+      )
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   return (
     <View>
       <Text>FIRE BASE</Text>
+      <Button title="sgin in" onPress={handleSignIn} />
+      <Button title="sign up" onPress={handleAuth} />
     </View>
   );
 };
